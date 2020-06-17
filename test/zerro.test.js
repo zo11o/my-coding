7d30d27c0ec90a3a192f47a67ec76958a38ce9ea
zo11o <zhuowenjian@inboyu.com>
2020-06-17 17:02:19 +0800

    add: test-demo

---
+++ test/zerro.test.js
@@ -0,0 +1,8 @@
+test('zero', () => {
+    const z = 0;
+    expect(z).not.toBeNull();
+    expect(z).toBeDefined();
+    expect(z).not.toBeUndefined();
+    expect(z).not.toBeTruthy();
+    expect(z).toBeFalsy();
+});
