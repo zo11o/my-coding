ad52a82d0ae99b5f3cc534060f6a42cc624e2aaa
zo11o <zhuowenjian@inboyu.com>
2020-06-17 18:06:20 +0800

    config: jest setup success

---
+++ src/algorithm/sort/quickSort.js
@@ -0,0 +1,6 @@
+// 快速排序
+function quickSort(arr) {
+    return arr.sort((a, b) => a - b);
+}
+
+export default quickSort;
--- test/sum.test.js
+++ test/sum.test.js
@@ -1,4 +1,4 @@
-const sum = require('../demo/sum')
+import sum from '../demo/sum'
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1,2)).toBe(3);
