269c9d7cb38cb6f4b2fbd6e24c46a4a887489080
zo11o <372526745@qq.com>
2021-04-29 17:37:42 +0800

    feat: 去掉提交cz

--- src/js/base/curry.js
+++ src/js/base/curry.js
@@ -1,6 +1,6 @@
 // 本文介绍函数 《Javascript高级编程》第 22 章 - 高级技巧中的
-// 函数科里化
-// 函数科里化：用于创建已经设置好了一个或多个参数的函数。
+// 函数科里化: 是把接受多个参数的函数变为接收单个参数的函数，并且返回接收余下的参数且返回运行结果的新函数的技术
+// 作用：提高适用性，降低通用性
 // 实现：使用一个闭包返回一个函数。
 
 // 一、举个例子
@@ -15,11 +15,14 @@ function curriedAdd(num2) {
 console.log(add(2, 3)); // 5
 console.log(curriedAdd(3)); // 8
 
-// 二、通用实现
+// 二、提供适用性的通用实现
 function curry(fn) {
+    // 获取外部参数 例子中为 5
     var args = Array.prototype.slice.call(arguments, 1);
 
+    // 返回一个方法
     return function () {
+        // 这里的 arguments 为内部参数 例子中为 3
         var innerArgs = Array.prototype.slice.call(arguments),
             finalArgs = args.concat(innerArgs);
         return fn.apply(null, finalArgs);
@@ -30,6 +33,47 @@ function curry(fn) {
 var curriedAdd = curry(add, 5);
 console.log(curriedAdd(3)); //8
 
+
+// 三：延迟执行
+
+var add = function() {
+  // var _this = this, args = arguments
+
+  return function () {
+    if (!arguments.length) {
+      var sum = 0;
+      for (var i = 0, c; c = _args[i]; i++) {
+        sum += c
+        return sum
+      }
+    } else {
+      Array.prototype.push.apply(_args, arguments)
+      // 返回函数自身
+      return arguments.callee
+    }
+  }
+}
+
+// 四： 延迟执行通用方法
+function add (n1, n2) {
+  return n1+n2
+}
+
+var curry = function(fn) {
+  var _args = []
+  return function cb() {
+      if (arguments.length == 0) {
+          // 使用保留函数参数调用
+          return fn.apply(this, _args)
+      }
+      // 保留函数参数
+      Array.prototype.push.apply(_args, arguments);
+      return cb;
+  }
+}
+
+var cAdd = curry(add)(1)(2)()
+console.log(cAdd)
 //-----------------------------
 
 // 反科里化
--- src/js/base/rgba2num.js
+++ src/js/base/rgba2num.js
@@ -1,26 +1,24 @@
 /**
  * 实现一个方法，将 rgba(241, 112, 333) 转换为
+ * @param {String}
  */
 function rgba2num(str) {
   let reg = /rgb(?:a?)\((?:\s+)?(\d{1,3}),(?:\s+)?(\d{1,3}),(?:\s+)?(\d{1,3})\)/
   let match = str.match(reg)
   let res = ''
-
   if (!match) {
     return res
   }
-
   let [,v1,v2,v3] = match
   res = [v1,v2,v3].reduce((acc, cur)=> acc + Number(cur).toString(16).padStart(2, 0), '#')
-
   return res
 }
 
-let color = 'rgba(22,44,11)';
-let color2 = 'rgb(212,144,11)';
+// let color = 'rgba(22,44,11)';
+// let color2 = 'rgb(212,144,11)';
 
-let s1 = rgba2num(color)
-console.log(s1)
+// // let s1 = rgba2num(color)
+// // console.log(s1)
 
-let s2 = rgba2num(color2)
-console.log(s2)
+// // let s2 = rgba2num(color2)
+// // console.log(s2)
